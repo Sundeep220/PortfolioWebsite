01ca89899f2ece4a65c4e2271b4ea562a2ae22ab
Sundeep220 <sundeepkumarsingh0@gmail.com>
2022-11-25 07:22:56 +0530

    Added skills section

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,7 +10,28 @@
 
 
 /*=============== SKILLS TABS ===============*/
+const tabs = document.querySelectorAll('[data-target]')
+const tabcontents = document.querySelectorAll('[data-content]')
 
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target)
+
+        tabcontents.forEach(tabcontent => {
+            tabcontent.classList.remove('skills-active')
+        })
+
+        target.classList.add('skills-active')
+
+
+        tabs.forEach(tab => {
+            tab.classList.remove('skills-active')
+        })
+
+        tab.classList.add('skills-active')
+    })
+})
 
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
 
