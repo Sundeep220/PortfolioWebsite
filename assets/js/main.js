053c3f4c433f38f02adf5c49d1904ccc22aa9725
Sundeep220 <sundeepkumarsingh0@gmail.com>
2022-12-13 13:22:05 +0530

    Added contact form

--- assets/js/main.js
+++ assets/js/main.js
@@ -81,7 +81,24 @@ function portfolioItemDetails(portfolioItem){
 
 
 /*=============== INPUT ANIMATION ===============*/
+const inputs = document.querySelectorAll(".input");
 
+function focusFun(){
+    let parent = this.parentNode;
+    parent.classList.add("focus");
+}
+
+function blurFun(){
+    let parent = this.parentNode;
+    if(this.value == ""){
+        parent.classList.remove("focus")
+    }
+}
+
+inputs.forEach((input) => {
+    input.addEventListener("focus", focusFun);
+    input.addEventListener("blur", blurFun);
+})
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
